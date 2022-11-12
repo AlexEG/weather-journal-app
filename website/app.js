680f979ed78a6b5f93ca413fa85517d7076936ee
Alexander EG#9070 <116421926+AlexEG@users.noreply.github.com>
2022-11-12 11:41:39 +0200

    Update app.js

--- website/app.js
+++ website/app.js
@@ -1,5 +1,8 @@
 /* Global Variables */
+let yourFeeling = document.getElementById('feelings').vslue
+let zipCode = document.getElementById('zip').vslue
 
 // Create a new date instance dynamically with JS
 let d = new Date();
 let newDate = d.getMonth()+'.'+ d.getDate()+'.'+ d.getFullYear();
+
