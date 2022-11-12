be02dd7d447bad36241f393ab5ece9bde6bc62a1
Alexander EG#9070 <116421926+AlexEG@users.noreply.github.com>
2022-11-12 08:44:24 +0200

    Update server.js

--- server.js
+++ server.js
@@ -1,24 +1,27 @@
+console.log(`hello`);
+
 // Setup empty JS object to act as endpoint for all routes
+
 projectData = {};
 
 // Express to run server and routes
-const express = require('express');
+const express = require("express");
 // Start up an instance of app
 const app = express();
 
+//Create a server running on the port 
+app.listen(3000)
 
 /* Middleware*/
-const bodyParser = require('body-parser')
+const bodyParser = require("body-parser");
 //Here we are configuring express to use body-parser as middle-ware.
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 // Cors for cross origin allowance
-const cors = require('cors');
+const cors = require("cors");
 app.use(cors());
 // Initialize the main project folder
-app.use(express.static('website'));
-
+app.use(express.static("website"));
 
 // Setup Server
-
