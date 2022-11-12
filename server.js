cf91b14ed4d632dd1cb3bc77a1c502afbb1a0e24
Alexander EG#9070 <116421926+AlexEG@users.noreply.github.com>
2022-11-12 08:21:41 +0200

    Update server.js

--- server.js
+++ server.js
@@ -8,12 +8,14 @@ const app = express();
 
 
 /* Middleware*/
+const bodyParser = require('body-parser')
 //Here we are configuring express to use body-parser as middle-ware.
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 // Cors for cross origin allowance
-
+const cors = require('cors');
+app.use(cors());
 // Initialize the main project folder
 app.use(express.static('website'));
 
