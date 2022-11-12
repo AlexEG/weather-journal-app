5df3681f291e6fb8e3242fbebd817651026c9653
Alexander EG#9070 <116421926+AlexEG@users.noreply.github.com>
2022-11-12 05:39:34 +0200

    create notes.js & tests.js

--- tests.js
+++ tests.js
@@ -1,18 +1,17 @@
 //instal cors sorce-- https://www.npmjs.com/package/cors
 
-var express = require('express')
-var cors = require('cors')
-var app = express()
- 
-app.use(cors())
- 
+var express = require("express");
+var cors = require("cors");
+var app = express();
 
-app.get('/products/:id', function (req, res, next) {
-  res.json({msg: 'This is CORS-enabled for all origins!'})
-})
- 
+app.use(cors());
+
+app.get("/products/:id", function (req, res, next) {
+  res.json({ msg: "This is CORS-enabled for all origins!" });
+});
+
 app.listen(80, function () {
-  console.log('CORS-enabled web server listening on port 80')
-})
+  console.log("CORS-enabled web server listening on port 80");
+});
 
 /*==========================*/
