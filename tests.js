ddee61cbabc84121da9db781ebb0e9446c54226f
Alexander EG#9070 <116421926+AlexEG@users.noreply.github.com>
2022-11-12 10:05:24 +0200

    Update tests.js

--- tests.js
+++ tests.js
@@ -1,32 +1,30 @@
-
-
 /*==========================*/
-
 projectData = {};
 
 const express = require("express");
-
 const app = express();
-
 app.listen(3050)
 
 /********Middleware*******/
 const bodyParser = require("body-parser");
 
-
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-
 const cors = require("cors");
 app.use(cors());
-
-
 app.use(express.static("website"));
+
+
 
 let getInfo = (req, res) => res.status(200).send(projectData);
 
-app.get('/', (req, res) => {
+app.get("/all", getInfo);
+
+
+
+
+  app.get('/', (req, res) => {
   console.log(`here line 23 `);
   res.sendStatus(200);
   
