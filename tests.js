f0b4f1576ce56bcc91f3c4c78bb976a0ca543cac
Alexander EG#9070 <116421926+AlexEG@users.noreply.github.com>
2022-11-12 10:26:17 +0200

    update

--- tests.js
+++ tests.js
@@ -1,16 +1,11 @@
 /*==========================*/
 projectData = {};
-
 const express = require("express");
 const app = express();
-app.listen(3050)
-
 /********Middleware*******/
 const bodyParser = require("body-parser");
-
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
-
 const cors = require("cors");
 app.use(cors());
 app.use(express.static("website"));
@@ -21,12 +16,18 @@ let getInfo = (req, res) => res.status(200).send(projectData);
 
 app.get("/all", getInfo);
 
+const port = 8000;
+const server = app.listen(port, ()=>{console.log(`running on localhost: ${port}`)})
 
+function listening(){
+    console.log("server running"); 
+    console.log(`running on localhost: {$port}`);
+}
 
+function one (){
+    server
+}
 
-
-
-
-
+one()
 /*==========================*/
 
