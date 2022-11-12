0ac4906bb3b1891b3c36b4da8aab990be1cd24e3
Alexander EG#9070 <116421926+AlexEG@users.noreply.github.com>
2022-11-12 09:43:11 +0200

    Update tests.js

--- tests.js
+++ tests.js
@@ -1,60 +1,33 @@
-/*
-let express = require("express");
-let cors = require("cors");
-let app = express();
-
-app.use(cors());
-
-app.get("/products/:id", function (req, res, next) {
-  res.json({ msg: "This is CORS-enabled for all origins!" });
-});
-
-app.listen(80, function () {
-  console.log("CORS-enabled web server listening on port 80");
-});
-*/
 
 /*==========================*/
+
 projectData = {};
 
 const express = require("express");
 
 const app = express();
 
-const cors = require("cors");
+app.listen(3000)
 
+/********Middleware*******/
 const bodyParser = require("body-parser");
 
-app.use(cors());
-
-// -----------------------------------//
 
 app.use(bodyParser.urlencoded({ extended: false }));
-
 app.use(bodyParser.json());
 
 
+const cors = require("cors");
+app.use(cors());
+
+
+app.use(express.static("website"));
+
+
+// Setup Server
+/*==========================*/
 
 app.get('/', (req, res) => {
 console.log(`here line 23 `);
 res.sendStatus(200);
 });
-
-app.listen(3050);
-
-
-/*
-const bodyParser = require("body-parser");
-
-app.use(bodyParser.urlencoded({ extended: false }));
-
-app.use(bodyParser.json());
-
-const cors = require("cors");
-
-app.use(cors());
-
-app.use(express.static("website"));
-
-*/
-
