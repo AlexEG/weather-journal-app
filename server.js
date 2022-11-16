f892d7dedf1cb88119440f772a98789300448d25
Alexander EG#9072 <116421926+AlexEG@users.noreply.github.com>
2022-11-16 10:39:52 +0200

    Update server.js
    post & get request

--- server.js
+++ server.js
@@ -9,18 +9,27 @@ const app = express();
 /* Middleware*/
 const bodyParser = require("body-parser");
 //Here we are configuring express to use body-parser as middle-ware.
-app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+// Initialize the main project folder
+app.use(express.static("website"));
 
 // Cors for cross origin allowance
 const cors = require("cors");
 app.use(cors());
 
-// Initialize the main project folder
-app.use(express.static("website"));
+/*===================POST=====================*/
+app.post('/add', async (req, res) => {
+  const info = await req.body;
+  projectData = info;
+  res.send(projectData);
+});
 /*===================GET=====================*/
-// app.get("/all", (req, res) => {
-//     res.status(200).send(projectData)});
+app.get("/all", async (req, res) => {
+  if(projectData){
+      res.send(projectData);
+  }
+});
 /*==============local server==============*/
 //Create a server running on the port
 //callback function with arrow function
